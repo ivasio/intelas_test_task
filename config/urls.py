23860fb4ae5e3b534193a10aa4f8b6b647be9d9f
ivasio <van.chebotar@yandex.ru>
2022-03-16 18:41:26 +0100

    hide unnecessary stuff

--- config/urls.py
+++ config/urls.py
@@ -7,15 +7,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
-    path(
-        "about/", TemplateView.as_view(template_name="pages/about.html"), name="about"
-    ),
-    # Django Admin, use {% url 'admin:index' %}
-    path(settings.ADMIN_URL, admin.site.urls),
-    # User management
-    path("users/", include("intelas_test_task.users.urls", namespace="users")),
-    path("accounts/", include("allauth.urls")),
-    # Your stuff: custom urls includes go here
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
@@ -40,7 +31,7 @@ if settings.DEBUG:
         ),
         path("500/", default_views.server_error),
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
-
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    # if "debug_toolbar" in settings.INSTALLED_APPS:
+    #     import debug_toolbar
+    #
+    #     urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
